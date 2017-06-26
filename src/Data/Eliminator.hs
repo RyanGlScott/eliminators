5da0b16b3eded381c26e1adf9c93801b7e9f688a
Ryan Scott <ryan.gl.scott@gmail.com>
2017-06-26 12:33:03 -0400

    Add defunctionalized versions

--- src/Data/Eliminator.hs
+++ src/Data/Eliminator.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE TypeOperators #-}
 {-|
@@ -28,21 +30,48 @@ boolElim :: forall (p :: Bool -> Type) (b :: Bool).
 boolElim SFalse pF _  = pF
 boolElim STrue  _  pT = pT
 
+boolElimTyFun :: forall (p :: Bool ~> Type) (b :: Bool).
+                 Sing b
+              -> p @@ False
+              -> p @@ True
+              -> p @@ b
+boolElimTyFun SFalse pF _  = pF
+boolElimTyFun STrue  _  pT = pT
+
 listElim :: forall (a :: Type) (p :: [a] -> Type) (l :: [a]).
             Sing l
          -> p '[]
          -> (forall (x :: a) (xs :: [a]). Sing x -> Sing xs -> p xs -> p (x:xs))
          -> p l
-listElim SNil         pNil _     = pNil
-listElim (SCons x xs) pNil pCons = pCons x xs (listElim xs pNil pCons)
+listElim SNil                      pNil _     = pNil
+listElim (SCons x (xs :: Sing xs)) pNil pCons = pCons x xs (listElim @a @p @xs xs pNil pCons)
+
+listElimTyFun :: forall (a :: Type) (p :: [a] ~> Type) (l :: [a]).
+                 Sing l
+              -> p @@ '[]
+              -> (forall (x :: a) (xs :: [a]). Sing x -> Sing xs -> p @@ xs -> p @@ (x:xs))
+              -> p @@ l
+listElimTyFun SNil                      pNil _     = pNil
+listElimTyFun (SCons x (xs :: Sing xs)) pNil pCons = pCons x xs (listElimTyFun @a @p @xs xs pNil pCons)
 
 natElim :: forall (p :: Nat -> Type) (n :: Nat).
            Sing n
         -> p 0
-        -> (forall k. Sing k -> p k -> p (k :+ 1))
+        -> (forall (k :: Nat). Sing k -> p k -> p (k :+ 1))
         -> p n
 natElim snat pZ pS =
   case fromSing snat of
     0        -> unsafeCoerce pZ
     nPlusOne -> case toSing (pred nPlusOne) of
-                  SomeSing sn -> unsafeCoerce (pS sn (natElim sn pZ pS))
+                  SomeSing (sn :: Sing k) -> unsafeCoerce (pS sn (natElim @p @k sn pZ pS))
+
+natElimTyFun :: forall (p :: Nat ~> Type) (n :: Nat).
+                Sing n
+             -> p @@ 0
+             -> (forall (k :: Nat). Sing k -> p @@ k -> p @@ (k :+ 1))
+             -> p @@ n
+natElimTyFun snat pZ pS =
+  case fromSing snat of
+    0        -> unsafeCoerce pZ
+    nPlusOne -> case toSing (pred nPlusOne) of
+                  SomeSing (sn :: Sing k) -> unsafeCoerce (pS sn (natElimTyFun @p @k sn pZ pS))
