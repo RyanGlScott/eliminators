fbd01df0ab344e22da000faa459e888b123ef31a
Ryan Scott <ryan.gl.scott@gmail.com>
2017-07-01 20:25:27 -0700

    ListSpec

---
+++ tests/ListSpec.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeOperators #-}
+module ListSpec where
+
+import Data.Eliminator
+import Data.Kind
+import Data.Singletons.Prelude
+import Data.Singletons.Prelude.List
+import Data.Type.Equality
+
+import EqualitySpec (congTyFun)
+
+import ListTypes
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = pure ()
+
+-----
+
+mapPreservesLength :: forall (x :: Type) (y :: Type) (f :: x ~> y) (l :: [x]).
+                      SingI l
+                   => Length l :~: Length (Map f l)
+mapPreservesLength
+  = elimListTyFun @x @(WhyMapPreservesLengthSym1 f) @l (sing @_ @l) base step
+  where
+    base :: WhyMapPreservesLength f '[]
+    base = Refl
+
+    step :: forall (s :: x) (ss :: [x]).
+            Sing s -> Sing ss
+         -> WhyMapPreservesLength f ss
+         -> WhyMapPreservesLength f (s:ss)
+    step _ _ = congTyFun @_ @_ @((:+$$) 1)
+
+mapFusion :: forall (x :: Type) (y :: Type) (z :: Type)
+                    (f :: y ~> z) (g :: x ~> y) (l :: [x]).
+                    SingI l
+                 => Map f (Map g l) :~: Map (f :.$$$ g) l
+mapFusion
+  = elimListTyFun @x @(WhyMapFusionSym2 f g) @l (sing @_ @l) base step
+  where
+    base :: WhyMapFusion f g '[]
+    base = Refl
+
+    step :: forall (s :: x) (ss :: [x]).
+            Sing s -> Sing ss
+         -> WhyMapFusion f g ss
+         -> WhyMapFusion f g (s:ss)
+    step _ _ = congTyFun @_ @_ @((:$$) (f @@ (g @@ s)))
---
+++ tests/ListTypes.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+module ListTypes where
+
+import Data.Singletons.Prelude
+import Data.Singletons.Prelude.List
+import Data.Singletons.TH
+
+type WhyMapPreservesLength (f :: x ~> y) (l :: [x])
+  = Length l :~: Length (Map f l)
+$(genDefunSymbols [''WhyMapPreservesLength])
+
+type WhyMapFusion (f :: y ~> z) (g :: x ~> y) (l :: [x])
+  = Map f (Map g l) :~: Map (f :.$$$ g) l
+$(genDefunSymbols [''WhyMapFusion])
