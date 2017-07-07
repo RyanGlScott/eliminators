d0e1fec3933fc4e44bb7e9cb1b0f7f74cb23dff5
Ryan Scott <ryan.gl.scott@gmail.com>
2017-07-07 18:46:27 -0400

    GHC Trac #13879 has been fixed

--- tests/EqualitySpec.hs
+++ tests/EqualitySpec.hs
@@ -74,16 +74,11 @@ instance SingI HRefl where
          -> p r
 (->:~~:) SHRefl pHRefl = pHRefl
 
-{-
-This doesn't typecheck at the moment due to GHC Trac #13879.
-TODO: Uncomment this when the fix becomes available.
-
 (~>:~~:) :: forall (j :: Type) (k :: Type) (a :: j) (b :: k) (r :: a :~~: b) (p :: forall (z :: Type) (y :: z). a :~~: y ~> Type).
             Sing r
          -> p @@ HRefl
          -> p @@ r
 (~>:~~:) SHRefl pHRefl = pHRefl
--}
 
 -- (-?>:~~:)
 
